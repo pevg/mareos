e9fef7200912a57069d147c106bd2c51abb62e04
pevg <pablove@epidataconsulting.com>
2024-08-06 23:54:29 -0300

    initial commit

---
+++ src/main/java/com/pevg/www/envios/MareoEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.pevg.www.envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareoEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MareoEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pevg/www/envios/MareoEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pevg.www.envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MareoEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
